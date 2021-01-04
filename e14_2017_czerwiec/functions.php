2b7dac6e7131d7d2f1499b43e9b3bd9557299474
Kristof-Ivan <76968657+Kristof-Ivan@users.noreply.github.com>
2021-01-04 20:03:38 +0100

    Update functions.php

--- e14_2017_czerwiec/functions.php
+++ e14_2017_czerwiec/functions.php
@@ -23,28 +23,3 @@ function PacjenciToList(array $dane):string{
     }
     return $html."</p>";
 }
-function GetPacjenci2(){
-    if(isset($_POST['id'])){
-        $conn = Connect();
-        $id= $_POST['id'];
-        $sql1="SELECT imie, nazwisko,choroby_przewlekle, uczulenia FROM pacjenciWHERE id=$id";
-        $result = $conn->query($sql1);
-        $tab =[];
-        while($row = $result->fetch_row()){
-            $tab[] = $row;
-        }
-        $conn->close();
-        return $tab;
-    }else{
-        echo "NIE DZIAŁA";
-    }
-}
-function PacjenciToList2(array $tab){
-    $html = "<p>";
-    foreach($tab as $row){
-        $html .= "Imie i nazwisko: {$row[0]} {$row[1]}<br>";
-        $html .= "Choroby przewlekłe: {$row[2]}<br>";
-        $html .= "Uczulenia: {$row[3]}<br>";
-    }
-    return $html."</p>";
-}
